b789e982cd9ae0ead9d5d2cec6494dcb59009bc3
Dexalt142 <faturjackson14@gmail.com>
2020-08-25 00:47:10 +0700

    Bikin logincontroller

---
+++ app/Http/Controllers/api/Auth/LoginController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\api\Auth;
+
+use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $validator = $this->validator($request->all());
+
+        if($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => 'The given was invalid',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        try {
+            if(!$token = JWTAuth::attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Login failed',
+                    'errors' => [
+                        'email' => ['Email atau password salah']
+                    ]
+                ], 401);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Something went wrong'
+            ], 500);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Login success',
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param array $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'email' => ['required'],
+            'password' => ['required'],
+        ]);
+    }
+}

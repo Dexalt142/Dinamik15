0253e0f949779f389d2cb01f49d8c7db6b9fc3ad
Dexalt142 <faturjackson14@gmail.com>
2020-08-25 00:49:59 +0700

    Hapus vue related file

--- resources/views/user/auth/login.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('user.layouts.app')
-
-@section('title', 'Dinamik 15 - Login')
-
-@section('content')
-    <div id="app">
-        <login-page></login-page>
-    </div>
-@endsection
--- resources/views/user/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('user.layouts.app')
-
-@section('title', 'Dinamik 15 - Dashboard')
-
-@section('content')
-    <h1>Ini dashboard</h1>
-@endsection

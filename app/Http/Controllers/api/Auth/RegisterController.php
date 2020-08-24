bffb89b9f883b32041b433d8008296f9c9c87eb0
Dexalt142 <faturjackson14@gmail.com>
2020-08-25 00:47:26 +0700

    Bikin registercontroller

---
+++ app/Http/Controllers/api/Auth/RegisterController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers\api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Exception;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = $this->validator($request->all());
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => 'The given data was invalid',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        try {
+            $user = $this->create($request->all());
+    
+            if ($user) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Registration success',
+                    'user' => $user
+                ]);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Registration failed'
+            ], 500);
+        }
+
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        $messages = [
+            'required' => ':attribute tidak boleh kosong',
+            'min' => ':attribute minimal :min karakter',
+            'email.unique' => 'Email sudah digunakan',
+            'password.confirmed' => 'Konfirmasi password tidak sama'
+        ];
+
+        $attributes = [
+            'name' => 'Nama',
+            'email' => 'Email',
+            'password' => 'Password',
+            'password_confirmation' => 'Konfirmasi password'
+        ];
+
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ], $messages, $attributes);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}

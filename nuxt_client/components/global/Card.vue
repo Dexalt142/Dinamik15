a300b5a2d4c90134d9e9c287bce5c4eed0eedf18
Dexalt142 <faturjackson14@gmail.com>
2020-08-29 00:56:56 +0700

    Card component

--- nuxt_client/components/global/Card.vue
+++ nuxt_client/components/global/Card.vue
@@ -1,16 +1,30 @@
 <template>
-    <div class="card">
+    <div :class="{'card-white':color==='white'}" class="card">
         <div class="card-body">
+            <div v-if="title" class="card-title">{{ title }}</div>
             <slot />
         </div>
+        <LoadingSpinner v-if="loading" :class="{'loading-white':color==='white'}" :title="loading_title"></LoadingSpinner>
     </div>
 </template>
 
 <script>
+import LoadingSpinner from './LoadingSpinner';
 export default {
     name: 'Card',
-    props: [
-
-    ]
+    props: {
+        title: {
+            type: String
+        },
+        color: {
+            type: String
+        },
+        loading: {
+            type: Boolean
+        },
+        loading_title: {
+            type: String
+        }
+    }
 }
 </script>

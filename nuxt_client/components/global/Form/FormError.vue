e7fa37df68d54971dac1aabadb513b98db51bbeb
Dexalt142 <faturjackson14@gmail.com>
2020-08-29 00:56:45 +0700

    Form components

---
+++ nuxt_client/components/global/Form/FormError.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="invalid-feedback">
+        {{ message }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FormError',
+    props: {
+        message: {
+            type: String
+        }
+    }
+}
+</script>
---
+++ nuxt_client/components/global/Form/FormInput.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="form-group">
+        <label for="">{{ name }}</label>
+        <input :type="type" :class="{'is-invalid' : error}" class="form-control" ref="inputRef" :name="name" :value="value" :autocomplete="autocomplete" @input="updateValue()">
+        <FormError v-if="error" :message="error"></FormError>
+    </div>
+</template>
+
+<script>
+import FormError from './FormError';
+export default {
+    name: 'Card',
+    props: {
+        name: {
+            type: String
+        },
+        type: {
+            type: String,
+            required: true,
+            default: 'text'
+        },
+        error: {
+            type: String
+        },
+        autocomplete: {
+            type: Boolean
+        },
+        value: {}
+    },
+    components: {
+        FormError
+    },
+    methods: {
+        updateValue() {
+            this.$emit('input', this.$refs.inputRef.value);
+        }
+    }
+}
+</script>

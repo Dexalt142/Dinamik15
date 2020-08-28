8f642b19f459c3c6c4467aa5535799f6aa1d37a8
Dexalt142 <faturjackson14@gmail.com>
2020-08-29 00:57:12 +0700

    Loading component

---
+++ nuxt_client/components/global/LoadingSpinner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="loading">
+        <div class="spinner"></div>
+        <div class="title" v-if="title">{{ title }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoadingSpinner',
+    props: {
+        title: {
+            type: String
+        }
+    }
+}
+</script>

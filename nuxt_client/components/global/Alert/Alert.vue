f9660be75bfbf31ce5707a8d8b5050e0e297584b
Dexalt142 <faturjackson14@gmail.com>
2020-09-01 00:24:53 +0700

    Alert component

---
+++ nuxt_client/components/global/Alert/Alert.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="alert" :class="{'alert-dismissible fade show':dismiss, 'alert-success':color==='success', 'alert-primary':color==='primary', 'alert-info':color==='info', 'alert-danger':color==='danger', 'alert-warning':color==='warning'}">
+        {{ message }}
+        <button v-if="dismiss" type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Alert',
+    props: {
+        message: {
+            type: String
+        },
+        dismiss: {
+            type: Boolean
+        },
+        color: {
+            type: String
+        }
+    }
+}
+</script>

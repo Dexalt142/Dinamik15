152e61cd465d7b4378b32cf3ec708e9d73f9f645
Dexalt142 <faturjackson14@gmail.com>
2020-09-01 00:26:33 +0700

    Verification button in navbar

--- nuxt_client/components/Navbar.vue
+++ nuxt_client/components/Navbar.vue
@@ -30,13 +30,15 @@
                         <router-link :to="{name: 'register'}" class="nav-link">Register</router-link>
                     </li>
 
-                    <li class="nav-item" v-if="getAuthStatus">
+                    <li class="nav-item" v-if="getAuthStatus && userVerified">
                         <router-link :to="{name: 'dashboard'}" class="nav-link">Dashboard</router-link>
                     </li>
 
                     <li class="nav-item dropdown" v-if="getAuthStatus">
                         <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ getUserData().name }}</a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <router-link class="dropdown-item" :to="{name: 'verification'}" v-if="!userVerified">Verifikasi</router-link>
+                            <div class="dropdown-divider" v-if="!userVerified"></div>
                             <button class="dropdown-item" @click="submitLogout">Logout</button>
                         </div>
                     </li>
@@ -56,7 +58,9 @@ export default {
         submitLogout() {
             try {
                 this.$store.dispatch('auth/logout');
-                this.$router.push({name:'welcome'});
+                if(!this.isWelcomePage) {
+                    this.$router.push({name:'welcome'});
+                }
             } catch (e) {
 
             }
@@ -65,6 +69,9 @@ export default {
     computed: {
         getAuthStatus() {
             return this.$store.getters['auth/getAuthStatus'];
+        },
+        userVerified() {
+            return this.$store.getters['auth/userVerified'];
         },
         isWelcomePage() {
             return (this.$route.name === 'welcome');
